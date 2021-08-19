d82b34d3bd890a47872c4d453f6955a63a4e5305
Axel <axosar@gmail.com>
2021-08-19 14:58:21 -0500

    test

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { getWhyNextReasons } from "../lib/api";
 
-export default function Home() {
+export default function Home({ reasons }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,10 +14,21 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        
-
-
+        {reasons
+          .slice(0, reasons.length - 1)
+          .map(({ title, description, href }) => (
+            <a
+              key={title}
+              href={href}
+              target="_blank"
+            >
+              <h3 className="font-bold mb-2">{title}</h3>
+              <div dangerouslySetInnerHTML={{ __html: description }} />
+              <span className="text-blue-600 hover:text-blue-400 hover:underline mt-4 block">
+                Documentation →
+              </span>
+            </a>
+          ))}
       </main>
 
       <footer className={styles.footer}>
@@ -31,4 +43,18 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps(context) {
+  const reasons = await getWhyNextReasons();
+
+  return {
+    props: {
+      reasons,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every second
+    revalidate: 1, // In seconds
+  };
 }
